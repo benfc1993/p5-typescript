599b8943ee16c91687366b87b14663d4bf270301
Ben_Feldberg_Collins <benfeldbergcollins@hotmail.co.uk>
2023-06-01 17:56:20 +0100

    fix path aliases in build

--- example/Rect.ts
+++ example/Rect.ts
@@ -10,7 +10,7 @@ export class Rect extends Component {
     width!: number
 
     constructor(sketchInstance: Sketch) {
-        super(sketchInstance)
+        super(sketchInstance, 3)
     }
 
     onLoad(): void {
