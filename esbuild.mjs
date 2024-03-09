12856e288c1844c314194354c6833c51ae74beea
Ben Feldberg Collins <41189326+benfc1993@users.noreply.github.com>
2024-03-09 01:18:04 +0000

    fix bundle exports (#36)
    * fix bundle exports
    
    * bump version

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,15 +1,9 @@
-import { dtsPlugin } from 'esbuild-plugin-d.ts'
 import esbuild from 'esbuild'
 
 await esbuild.build({
     entryPoints: ['./src/index.ts'],
     outdir: './dist',
-    plugins: [
-        dtsPlugin({
-            // Optional options here
-        }),
-    ],
     bundle: true,
-    treeShaking: true,
     minify: true,
+    format: 'esm',
 })
